57e5f4888d6e1bbebc506ca1eaa226b9ffe922f8
Tejashwin U <tejashwin@outlook.com>
2022-01-28 18:26:11 +0530

    before major scroll removal

--- .qt_for_python/uic/posts.py
+++ .qt_for_python/uic/posts.py
@@ -14,170 +14,13 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_Form(object):
     def setupUi(self, Form):
         Form.setObjectName("Form")
-        Form.resize(563, 631)
+        Form.resize(563, 611)
         Form.setStyleSheet("")
         self.gridLayout = QtWidgets.QGridLayout(Form)
         self.gridLayout.setObjectName("gridLayout")
-        self.frame_2 = QtWidgets.QFrame(Form)
-        self.frame_2.setMinimumSize(QtCore.QSize(541, 291))
-        self.frame_2.setMaximumSize(QtCore.QSize(541, 291))
-        self.frame_2.setStyleSheet("#frame_2{/* Auto layout */\n"
-"\n"
-"display: flex;\n"
-"flex-direction: column;\n"
-"align-items: flex-start;\n"
-"padding: 40px 25px;\n"
-"\n"
-"position: absolute;\n"
-"width: 593px;\n"
-"height: 594px;\n"
-"left: calc(50% - 593px/2 - 0.5px);\n"
-"top: 243px;\n"
-"\n"
-"/* White */\n"
-"\n"
-"background: #FFFFFF;\n"
-"/* Grey / Dark */\n"
-"\n"
-"border: 1px solid #D1D1D1;\n"
-"box-sizing: border-box;\n"
-"border-radius: 8px;}")
-        self.frame_2.setFrameShape(QtWidgets.QFrame.StyledPanel)
-        self.frame_2.setFrameShadow(QtWidgets.QFrame.Raised)
-        self.frame_2.setObjectName("frame_2")
-        self.post_heading_posts = QtWidgets.QLabel(self.frame_2)
-        self.post_heading_posts.setGeometry(QtCore.QRect(20, 30, 171, 31))
-        font = QtGui.QFont()
-        font.setFamily("Poppins")
-        font.setPointSize(-1)
-        font.setBold(False)
-        font.setItalic(False)
-        font.setWeight(50)
-        self.post_heading_posts.setFont(font)
-        self.post_heading_posts.setStyleSheet("position: absolute;\n"
-"width: 233px;\n"
-"height: 60px;\n"
-"left: 92px;\n"
-"top: 48px;\n"
-"\n"
-"font-family: Poppins;\n"
-"font-style: normal;\n"
-"font-weight: normal;\n"
-"font-size: 25px;\n"
-"line-height: 60px;\n"
-"/* identical to box height */\n"
-"\n"
-"letter-spacing: 0.05em;\n"
-"text-decoration-line: underline;\n"
-"text-transform: capitalize;\n"
-"\n"
-"color: #000000;\n"
-"")
-        self.post_heading_posts.setObjectName("post_heading_posts")
-        self.des = QtWidgets.QLabel(self.frame_2)
-        self.des.setGeometry(QtCore.QRect(20, 70, 511, 131))
-        self.des.setMaximumSize(QtCore.QSize(511, 16777215))
-        self.des.setStyleSheet("background: rgba(196, 196, 196, 0.1);\n"
-"font-family: Poppins;\n"
-"font-style: normal;\n"
-"font-weight: normal;\n"
-"font-size: 15px;\n"
-"line-height: 30px;\n"
-"/* identical to box height */\n"
-"\n"
-"letter-spacing: 0.05em;\n"
-"text-transform: capitalize;\n"
-"\n"
-"color: #000000;")
-        self.des.setScaledContents(False)
-        self.des.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignTop)
-        self.des.setWordWrap(True)
-        self.des.setObjectName("des")
-        self.label = QtWidgets.QLabel(self.frame_2)
-        self.label.setGeometry(QtCore.QRect(20, 250, 111, 31))
-        self.label.setStyleSheet("font-family: Poppins;\n"
-"font-style: normal;\n"
-"font-weight: normal;\n"
-"font-size: 15px;\n"
-"line-height: 30px;\n"
-"/* identical to box height */\n"
-"\n"
-"letter-spacing: 0.05em;\n"
-"text-transform: capitalize;\n"
-"")
-        self.label.setObjectName("label")
-        self.pushButton = QtWidgets.QPushButton(self.frame_2)
-        self.pushButton.setGeometry(QtCore.QRect(412, 250, 121, 31))
-        icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap("c:\\Users\\tejas\\Desktop\\22-01-22\\assests/icons/plus.svg"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        self.pushButton.setIcon(icon)
-        self.pushButton.setObjectName("pushButton")
-        self.lineEdit = QtWidgets.QLineEdit(self.frame_2)
-        self.lineEdit.setGeometry(QtCore.QRect(152, 250, 251, 31))
-        self.lineEdit.setStyleSheet("position: static;\n"
-"width: 535px;\n"
-"height: 53px;\n"
-"left: 0px;\n"
-"top: 0px;\n"
-"\n"
-"/* White */\n"
-"\n"
-"background: #FFFFFF;\n"
-"/* Dark / Medium */\n"
-"\n"
-"border: 2px solid #4F4F4F;\n"
-"box-sizing: border-box;\n"
-"border-radius: 7px;\n"
-"\n"
-"")
-        self.lineEdit.setObjectName("lineEdit")
-        self.label_2 = QtWidgets.QLabel(self.frame_2)
-        self.label_2.setGeometry(QtCore.QRect(30, 210, 391, 31))
-        self.label_2.setStyleSheet("font-family: Poppins;\n"
-"font-style: normal;\n"
-"font-weight: normal;\n"
-"font-size: 15px;\n"
-"line-height: 30px;\n"
-"/* identical to box height */\n"
-"\n"
-"letter-spacing: 0.05em;\n"
-"text-transform: capitalize;\n"
-"")
-        self.label_2.setObjectName("label_2")
-        self.pushButton_2 = QtWidgets.QPushButton(self.frame_2)
-        self.pushButton_2.setGeometry(QtCore.QRect(430, 210, 93, 28))
-        self.pushButton_2.setObjectName("pushButton_2")
-        self.label_3 = QtWidgets.QLabel(self.frame_2)
-        self.label_3.setGeometry(QtCore.QRect(10, 10, 201, 16))
-        self.label_3.setStyleSheet("font-family: Poppins;\n"
-"font-style: normal;\n"
-"font-weight: normal;\n"
-"font-size: 13px;\n"
-"line-height: 30px;\n"
-"/* identical to box height */\n"
-"\n"
-"letter-spacing: 0.05em;\n"
-"text-transform: capitalize;\n"
-"")
-        self.label_3.setObjectName("label_3")
-        self.close_button_posts = QtWidgets.QPushButton(self.frame_2)
-        self.close_button_posts.setGeometry(QtCore.QRect(490, 10, 40, 40))
-        self.close_button_posts.setStyleSheet("*{border-radius:0px;}\n"
-":pressed{\n"
-"    border: 3px solid  #FF0000;\n"
-"boder:10px;\n"
-"border-radius: 8px;\n"
-"}")
-        self.close_button_posts.setText("")
-        icon1 = QtGui.QIcon()
-        icon1.addPixmap(QtGui.QPixmap("c:\\Users\\tejas\\Desktop\\22-01-22\\assests/icons/x-square.svg"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        self.close_button_posts.setIcon(icon1)
-        self.close_button_posts.setIconSize(QtCore.QSize(40, 40))
-        self.close_button_posts.setObjectName("close_button_posts")
-        self.gridLayout.addWidget(self.frame_2, 0, 0, 1, 1)
         self.assign_scroll_area = QtWidgets.QScrollArea(Form)
-        self.assign_scroll_area.setMinimumSize(QtCore.QSize(539, 291))
-        self.assign_scroll_area.setMaximumSize(QtCore.QSize(539, 291))
+        self.assign_scroll_area.setMinimumSize(QtCore.QSize(541, 290))
+        self.assign_scroll_area.setMaximumSize(QtCore.QSize(541, 291))
         self.assign_scroll_area.setStyleSheet("background: #FFFFFF;\n"
 "border-radius: 8px;")
         self.assign_scroll_area.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
@@ -185,7 +28,7 @@ class Ui_Form(object):
         self.assign_scroll_area.setWidgetResizable(True)
         self.assign_scroll_area.setObjectName("assign_scroll_area")
         self.assign_scroll_area_widget = QtWidgets.QWidget()
-        self.assign_scroll_area_widget.setGeometry(QtCore.QRect(0, 0, 539, 323))
+        self.assign_scroll_area_widget.setGeometry(QtCore.QRect(0, 0, 541, 323))
         self.assign_scroll_area_widget.setStyleSheet("text-align: center;\n"
 "letter-spacing: 0.05em;\n"
 "\n"
@@ -459,6 +302,163 @@ class Ui_Form(object):
         self.verticalLayout_3.addWidget(self.events_view_3)
         self.assign_scroll_area.setWidget(self.assign_scroll_area_widget)
         self.gridLayout.addWidget(self.assign_scroll_area, 1, 0, 1, 1)
+        self.frame_2 = QtWidgets.QFrame(Form)
+        self.frame_2.setMinimumSize(QtCore.QSize(541, 291))
+        self.frame_2.setMaximumSize(QtCore.QSize(541, 291))
+        self.frame_2.setStyleSheet("#frame_2{/* Auto layout */\n"
+"\n"
+"display: flex;\n"
+"flex-direction: column;\n"
+"align-items: flex-start;\n"
+"padding: 40px 25px;\n"
+"\n"
+"position: absolute;\n"
+"width: 593px;\n"
+"height: 594px;\n"
+"left: calc(50% - 593px/2 - 0.5px);\n"
+"top: 243px;\n"
+"\n"
+"/* White */\n"
+"\n"
+"background: #FFFFFF;\n"
+"/* Grey / Dark */\n"
+"\n"
+"border: 1px solid #D1D1D1;\n"
+"box-sizing: border-box;\n"
+"border-radius: 8px;}")
+        self.frame_2.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame_2.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame_2.setObjectName("frame_2")
+        self.post_heading_posts = QtWidgets.QLabel(self.frame_2)
+        self.post_heading_posts.setGeometry(QtCore.QRect(20, 30, 171, 31))
+        font = QtGui.QFont()
+        font.setFamily("Poppins")
+        font.setPointSize(-1)
+        font.setBold(False)
+        font.setItalic(False)
+        font.setWeight(50)
+        self.post_heading_posts.setFont(font)
+        self.post_heading_posts.setStyleSheet("position: absolute;\n"
+"width: 233px;\n"
+"height: 60px;\n"
+"left: 92px;\n"
+"top: 48px;\n"
+"\n"
+"font-family: Poppins;\n"
+"font-style: normal;\n"
+"font-weight: normal;\n"
+"font-size: 25px;\n"
+"line-height: 60px;\n"
+"/* identical to box height */\n"
+"\n"
+"letter-spacing: 0.05em;\n"
+"text-decoration-line: underline;\n"
+"text-transform: capitalize;\n"
+"\n"
+"color: #000000;\n"
+"")
+        self.post_heading_posts.setObjectName("post_heading_posts")
+        self.des = QtWidgets.QLabel(self.frame_2)
+        self.des.setGeometry(QtCore.QRect(20, 70, 511, 131))
+        self.des.setMaximumSize(QtCore.QSize(511, 16777215))
+        self.des.setStyleSheet("background: rgba(196, 196, 196, 0.1);\n"
+"font-family: Poppins;\n"
+"font-style: normal;\n"
+"font-weight: normal;\n"
+"font-size: 15px;\n"
+"line-height: 30px;\n"
+"/* identical to box height */\n"
+"\n"
+"letter-spacing: 0.05em;\n"
+"text-transform: capitalize;\n"
+"\n"
+"color: #000000;")
+        self.des.setScaledContents(False)
+        self.des.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignTop)
+        self.des.setWordWrap(True)
+        self.des.setObjectName("des")
+        self.label = QtWidgets.QLabel(self.frame_2)
+        self.label.setGeometry(QtCore.QRect(20, 250, 111, 31))
+        self.label.setStyleSheet("font-family: Poppins;\n"
+"font-style: normal;\n"
+"font-weight: normal;\n"
+"font-size: 15px;\n"
+"line-height: 30px;\n"
+"/* identical to box height */\n"
+"\n"
+"letter-spacing: 0.05em;\n"
+"text-transform: capitalize;\n"
+"")
+        self.label.setObjectName("label")
+        self.pushButton = QtWidgets.QPushButton(self.frame_2)
+        self.pushButton.setGeometry(QtCore.QRect(412, 250, 121, 31))
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("c:\\Users\\tejas\\Desktop\\22-01-22\\assests/icons/plus.svg"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.pushButton.setIcon(icon)
+        self.pushButton.setObjectName("pushButton")
+        self.lineEdit = QtWidgets.QLineEdit(self.frame_2)
+        self.lineEdit.setGeometry(QtCore.QRect(152, 250, 251, 31))
+        self.lineEdit.setStyleSheet("position: static;\n"
+"width: 535px;\n"
+"height: 53px;\n"
+"left: 0px;\n"
+"top: 0px;\n"
+"\n"
+"/* White */\n"
+"\n"
+"background: #FFFFFF;\n"
+"/* Dark / Medium */\n"
+"\n"
+"border: 2px solid #4F4F4F;\n"
+"box-sizing: border-box;\n"
+"border-radius: 7px;\n"
+"\n"
+"")
+        self.lineEdit.setObjectName("lineEdit")
+        self.label_2 = QtWidgets.QLabel(self.frame_2)
+        self.label_2.setGeometry(QtCore.QRect(30, 210, 391, 31))
+        self.label_2.setStyleSheet("font-family: Poppins;\n"
+"font-style: normal;\n"
+"font-weight: normal;\n"
+"font-size: 15px;\n"
+"line-height: 30px;\n"
+"/* identical to box height */\n"
+"\n"
+"letter-spacing: 0.05em;\n"
+"text-transform: capitalize;\n"
+"")
+        self.label_2.setObjectName("label_2")
+        self.pushButton_2 = QtWidgets.QPushButton(self.frame_2)
+        self.pushButton_2.setGeometry(QtCore.QRect(430, 210, 93, 28))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.label_3 = QtWidgets.QLabel(self.frame_2)
+        self.label_3.setGeometry(QtCore.QRect(10, 10, 201, 16))
+        self.label_3.setStyleSheet("font-family: Poppins;\n"
+"font-style: normal;\n"
+"font-weight: normal;\n"
+"font-size: 13px;\n"
+"line-height: 30px;\n"
+"/* identical to box height */\n"
+"\n"
+"letter-spacing: 0.05em;\n"
+"text-transform: capitalize;\n"
+"")
+        self.label_3.setObjectName("label_3")
+        self.close_button_posts = QtWidgets.QPushButton(self.frame_2)
+        self.close_button_posts.setGeometry(QtCore.QRect(490, 10, 40, 40))
+        self.close_button_posts.setStyleSheet("*{border-radius:0px;}\n"
+":pressed{\n"
+"    border: 3px solid  #FF0000;\n"
+"boder:10px;\n"
+"border-radius: 8px;\n"
+"}")
+        self.close_button_posts.setText("")
+        icon1 = QtGui.QIcon()
+        icon1.addPixmap(QtGui.QPixmap("c:\\Users\\tejas\\Desktop\\22-01-22\\assests/icons/x-square.svg"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.close_button_posts.setIcon(icon1)
+        self.close_button_posts.setIconSize(QtCore.QSize(40, 40))
+        self.close_button_posts.setObjectName("close_button_posts")
+        self.gridLayout.addWidget(self.frame_2, 0, 0, 1, 1)
 
         self.retranslateUi(Form)
         QtCore.QMetaObject.connectSlotsByName(Form)
@@ -466,13 +466,6 @@ class Ui_Form(object):
     def retranslateUi(self, Form):
         _translate = QtCore.QCoreApplication.translate
         Form.setWindowTitle(_translate("Form", "Form"))
-        self.post_heading_posts.setText(_translate("Form", "Post Heading"))
-        self.des.setText(_translate("Form", "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec."))
-        self.label.setText(_translate("Form", "Comments 🔽"))
-        self.pushButton.setText(_translate("Form", "Add comment"))
-        self.label_2.setText(_translate("Form", "Files: "))
-        self.pushButton_2.setText(_translate("Form", "Download"))
-        self.label_3.setText(_translate("Form", "Time"))
         self.label_8.setText(_translate("Form", "Comment"))
         self.label_9.setText(_translate("Form", "User"))
         self.label_14.setText(_translate("Form", "Comment"))
@@ -481,3 +474,10 @@ class Ui_Form(object):
         self.label_17.setText(_translate("Form", "User"))
         self.label_10.setText(_translate("Form", "Comment"))
         self.label_11.setText(_translate("Form", "User"))
+        self.post_heading_posts.setText(_translate("Form", "Post Heading"))
+        self.des.setText(_translate("Form", "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec."))
+        self.label.setText(_translate("Form", "Comments 🔽"))
+        self.pushButton.setText(_translate("Form", "Add comment"))
+        self.label_2.setText(_translate("Form", "Files: "))
+        self.pushButton_2.setText(_translate("Form", "Download"))
+        self.label_3.setText(_translate("Form", "Time"))
